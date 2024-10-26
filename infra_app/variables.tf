faaca9ed242602734813e6b63897510d99e4e4d8
Alvin V <alvin.vassoodaven@colibridigital.io>
2024-10-26 16:08:12 +0100

    updating  secrets for workflow

--- infra_app/variables.tf
+++ infra_app/variables.tf
@@ -2,3 +2,8 @@ variable "gcs_tf_bucket" {
   description = "The GCS bucket used for Terraform state"
   type        = string
 }
+
+variable "project_id" {
+  type        = string
+  description = "The Google Cloud Project ID"
+}

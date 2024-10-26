fb78aa248a1cc3bafa242884861a06d958001719
Alvin V <alvin.vassoodaven@colibridigital.io>
2024-10-26 17:28:15 +0100

    updating module

--- infra_app/gke.tf
+++ infra_app/gke.tf
@@ -6,40 +6,86 @@ provider "kubernetes" {
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
+
 module "gke" {
-  source                   = "squareops/kubernetes-engine/google"
-  project                  = var.project_id
-  name                     = "webapp"
-  region                   = "europe-west2"
-  environment              = "dev"
-  gke_zones                = ["europe-west2-a", "europe-west2-b"]
-  vpc_name                 = "gke-vpc"
-  subnet                   = "dev-subnet-1"
-  kubernetes_version       = "1.25"
-  default_np_instance_type = "e2-medium"
-  default_np_max_count     = 2
-  default_np_preemptible   = true
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  project_id                 = var.project_id
+  name                       = "webapp"
+  region                     = "europe-west2"
+  zones                      = ["europe-west2-a", "europe-west2-b"]
+  network                    = "gke-01"
+  subnetwork                 = "europe-west2-01"
+  ip_range_pods              = "webapp-01-pods"
+  ip_range_services          = "webapp-01-services"
+  http_load_balancing        = false
+  network_policy             = false
+  horizontal_pod_autoscaling = true
+  filestore_csi_driver       = false
+  dns_cache                  = false
 
-}
+  node_pools = [
+    {
+      name                        = "default-node-pool"
+      machine_type                = "e2-medium"
+      node_locations              = "europe-west2-a,europe-west2-b"
+      min_count                   = 1
+      max_count                   = 2
+      local_ssd_count             = 0
+      spot                        = false
+      disk_size_gb                = 100
+      disk_type                   = "pd-standard"
+      image_type                  = "COS_CONTAINERD"
+      enable_gcfs                 = false
+      enable_gvnic                = false
+      logging_variant             = "DEFAULT"
+      auto_repair                 = true
+      auto_upgrade                = true
+      service_account             = "project-service-account@${var.project_id}.iam.gserviceaccount.com"
+      preemptible                 = false
+      initial_node_count          = 0
+    },
+  ]
 
+  node_pools_oauth_scopes = {
+    all = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
 
-module "node_pool" {
-  source             = "squareops/kubernetes-engine/google//modules/node-pool"
-  depends_on         = [module.gke]
-  project            = project_name
-  name               = module.gke.name
-  environment        = "dev"
-  location           = "europe-west2"
-  kubernetes_version = "1.25"
-  service_account    = module.gke.service_accounts_gke
-  initial_node_count = 1
-  min_count          = 1
-  max_count          = 2
-  node_locations     = ["europe-west2-a", "europe-west2-b"]
-  preemptible        = true
-  instance_type      = "e2-medium"
-  disk_size_gb       = 50
-  labels = {
-    "App-services" : true
+  node_pools_labels = {
+    all = {}
+
+    default-node-pool = {
+      default-node-pool = true
+    }
+  }
+
+  node_pools_metadata = {
+    all = {}
+
+    default-node-pool = {
+      node-pool-metadata-custom-value = "webapp-node-pool"
+    }
+  }
+
+  node_pools_taints = {
+    all = []
+
+    default-node-pool = [
+      {
+        key    = "default-node-pool"
+        value  = true
+        effect = "PREFER_NO_SCHEDULE"
+      },
+    ]
+  }
+
+  node_pools_tags = {
+    all = []
+
+    default-node-pool = [
+      "default-node-pool",
+    ]
   }
 }

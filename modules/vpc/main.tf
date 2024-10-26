0a20b4e3b216c3d241b626ad5f7462f4afcb7552
Alvin V <alvin.vassoodaven@colibridigital.io>
2024-10-26 17:57:01 +0100

    adding network tfs

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,3 +4,12 @@ resource "google_compute_network" "this" {
   routing_mode                    = var.routing_mode
   delete_default_routes_on_create = var.delete_default_routes_on_create
 }
+
+resource "google_compute_subnetwork" "this" {
+  count                    = length(var.subnets)
+  name                     = var.subnets[count.index].name
+  ip_cidr_range            = var.subnets[count.index].ip_cidr_range
+  region                   = var.subnets[count.index].region
+  network                  = google_compute_network.this.self_link
+  private_ip_google_access = var.subnets[count.index].private_ip_google_access
+}

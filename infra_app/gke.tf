0a1416157364d28dbe6abf9002997fc97bb9c61f
godstaker <alvin.vassoodaven@gmail.com>
2024-10-26 18:10:11 +0100

    Revert "testing new config"

--- infra_app/gke.tf
+++ infra_app/gke.tf
@@ -1,26 +1,5 @@
-module "gcp_vpc" {
-  source = "../modules/gcp_vpc"
 
-  vpc_name        = "gke-vpc"
-  vpc_description = "for gke cluster"
-
-  subnets = [
-    {
-      name                     = "subnet-1"
-      ip_cidr_range            = "10.0.1.0/24"
-      region                   = var.region
-      private_ip_google_access = true
-    },
-    {
-      name                     = "subnet-2"
-      ip_cidr_range            = "10.0.2.0/24"
-      region                   = var.region
-      private_ip_google_access = false
-    },
-  ]
-}
-
-
+/**
 # google_client_config and kubernetes provider must be explicitly specified like the following.
 data "google_client_config" "default" {}
 
@@ -34,10 +13,10 @@ module "gke" {
   source                     = "terraform-google-modules/kubernetes-engine/google"
   project_id                 = var.project_id
   name                       = "webapp"
-  region                     = var.region
+  region                     = "europe-west2"
   zones                      = ["europe-west2-a", "europe-west2-b"]
-  network                    = module.gcp_vpc.network
-  subnetwork                 = "subnet-1"
+  network                    = "gke-01"
+  subnetwork                 = "europe-west2-01"
   ip_range_pods              = "webapp-01-pods"
   ip_range_services          = "webapp-01-services"
   http_load_balancing        = false
@@ -48,24 +27,24 @@ module "gke" {
 
   node_pools = [
     {
-      name               = "default-node-pool"
-      machine_type       = "e2-medium"
-      node_locations     = "europe-west2-a,europe-west2-b"
-      min_count          = 1
-      max_count          = 2
-      local_ssd_count    = 0
-      spot               = false
-      disk_size_gb       = 100
-      disk_type          = "pd-standard"
-      image_type         = "COS_CONTAINERD"
-      enable_gcfs        = false
-      enable_gvnic       = false
-      logging_variant    = "DEFAULT"
-      auto_repair        = true
-      auto_upgrade       = true
-      service_account    = "project-service-account@${var.project_id}.iam.gserviceaccount.com"
-      preemptible        = false
-      initial_node_count = 0
+      name                        = "default-node-pool"
+      machine_type                = "e2-medium"
+      node_locations              = "europe-west2-a,europe-west2-b"
+      min_count                   = 1
+      max_count                   = 2
+      local_ssd_count             = 0
+      spot                        = false
+      disk_size_gb                = 100
+      disk_type                   = "pd-standard"
+      image_type                  = "COS_CONTAINERD"
+      enable_gcfs                 = false
+      enable_gvnic                = false
+      logging_variant             = "DEFAULT"
+      auto_repair                 = true
+      auto_upgrade                = true
+      service_account             = "project-service-account@${var.project_id}.iam.gserviceaccount.com"
+      preemptible                 = false
+      initial_node_count          = 0
     },
   ]
 
@@ -111,6 +90,5 @@ module "gke" {
       "default-node-pool",
     ]
   }
-  depends_on = [module.gcp_vpc]
 }
 ### test###

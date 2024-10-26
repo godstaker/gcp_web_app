ef40c29c7ec7db21ff402e5bd45a56292eb4402a
Alvin V <alvin.vassoodaven@colibridigital.io>
2024-10-26 16:15:54 +0100

    updating gke cluster files

---
+++ infra_app/gke.tf
@@ -0,0 +1,10 @@
+module "gke_cluster" {
+  source            = "./modules/gke"
+  project_id        = var.project_id
+  region            = var.region
+  cluster_name      = "contoso"
+  network_name      = "gke-cluster"
+  subnet_name       = "webapp"
+  node_machine_type = var.node_machine_type
+  node_count        = 1
+}
--- infra_app/variables.tf
+++ infra_app/variables.tf
@@ -7,3 +7,15 @@ variable "project_id" {
   type        = string
   description = "The Google Cloud Project ID"
 }
+
+variable "region" {
+  type        = string
+  description = "The GCP region to deploy the cluster to"
+  default     = "europe-west2"
+}
+
+variable "node_machine_type" {
+  description = "Machine type for nodes in the default node pool"
+  type        = string
+  default     = "e2-medium"
+}

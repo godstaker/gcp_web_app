5d407c2a0d3bc664fe8411eb0ccb8f73304ee9f1
Alvin V <alvin.vassoodaven@colibridigital.io>
2024-10-26 16:43:35 +0100

    correcting tf files

--- infra_app/gke.tf
+++ infra_app/gke.tf
@@ -1,10 +1,16 @@
 module "gke_cluster" {
-  source            = "../modules/gke"
-  project_id        = var.project_id
-  region            = var.region
-  cluster_name      = "contoso"
-  network_name      = "gke-cluster"
-  subnet_name       = "webapp"
-  node_machine_type = var.node_machine_type
-  node_count        = 1
+  source                        = "../modules/gke"
+  project_id                    = var.project_id
+  region                        = var.region
+  cluster_name                  = "contoso"
+  network_name                  = "gke-cluster"
+  subnet_name                   = "webapp"
+  node_machine_type             = var.node_machine_type
+  node_count                    = 1
+  services_ipv4_cidr_block      = "10.0.0.0/26"
+  services_secondary_range_name = "services-secondary-range"
+  machine_type                  = var.machine_type
+  subnet_cidr_range             = "10.0.1.0/26"
+  cluster_ipv4_cidr_block       = "10.0.2.0/26"
+  cluster_secondary_range_name  = "cluster-secondary-range"
 }
--- infra_app/variables.tf
+++ infra_app/variables.tf
@@ -14,6 +14,12 @@ variable "region" {
   default     = "europe-west2"
 }
 
+variable "machine_type" {
+  type        = string
+  description = "The machine type for the cluster nodes"
+  default     = "e2-medium"
+}
+
 variable "node_machine_type" {
   description = "Machine type for nodes in the default node pool"
   type        = string

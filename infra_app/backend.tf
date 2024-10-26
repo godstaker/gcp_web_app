cbad85c3bac4908bdcf0f7cd986997de471a6ab0
Alvin V <alvin.vassoodaven@colibridigital.io>
2024-10-26 12:14:04 +0100

    updating backend

--- infra_app/backend.tf
+++ infra_app/backend.tf
@@ -1,6 +1,8 @@
 terraform {
+  required_version = ">= 1.0.0"  # Replace with your required version
+
   backend "gcs" {
     bucket = "av-webapp282265" # Replace with your GCS bucket name
-    prefix = "terraform/state" # Folder path within the bucket to store the state
+    prefix = "terraform/state"  # Folder path within the bucket to store the state
   }
 }
